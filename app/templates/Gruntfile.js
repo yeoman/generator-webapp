1e31faa71f2c73e9017a242fee3a080087843ca7
Sindre Sorhus <sindresorhus@gmail.com>
2013-02-16 00:31:42 +0100

    Also update port location in Mocha task

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -97,7 +97,7 @@ module.exports = function (grunt) {
             all: {
                 options: {
                     run: true,
-                    urls: ['http://localhost:<%%= connect.test.options.port %>/index.html']
+                    urls: ['http://localhost:<%%= connect.options.port %>/index.html']
                 }
             }
         },
