ba4c035313b4ff40d8eb1146da6264174689590c
Pascal Hartig <phartig@rdrei.net>
2013-02-23 12:57:48 +0100

    Tests to check for file creation

--- test/test.js
+++ test/test.js
@@ -1,30 +1,78 @@
+/*global describe beforeEach it*/
 
 var path    = require('path');
 var helpers = require('yeoman-generator').test;
 var assert  = require('assert');
 
 
-describe('Webapp generator test', function() {
-  before(helpers.before(path.join(__dirname, './temp')));
+describe('Webapp generator test', function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
 
-  it('the generator can be required without throwing', function() {
+      this.webapp = helpers.createGenerator('webapp:app', [
+        '../../app', [
+          helpers.createDummyGenerator(),
+          'mocha:app'
+        ]
+      ]);
+      done();
+    }.bind(this));
+  });
+
+  it('the generator can be required without throwing', function () {
     // not testing the actual run of generators yet
     this.app = require('../app');
   });
 
+  it('creates expected files in non-AMD mode', function (done) {
+    var expected = [
+      ['component.json', /"name": "temp"/],
+      ['package.json', /"name": "temp"/],
+      'Gruntfile.js',
+      'app/404.html',
+      'app/favicon.ico',
+      'app/robots.txt',
+      'app/index.html',
+      'app/scripts/hello.coffee',
+      'app/scripts/main.js',
+      'app/styles/main.scss'
+    ];
 
-  // FIXME
-  it.skip('creates expected files', function() {
+    helpers.mockPrompt(this.webapp, {
+      'compassBootstrap': 'Y',
+      'includeRequireJS': 'N'
+    });
 
-    // Use helpers.assertFile() to help you test the output of your generator
-    //
-    // Example:
-    //
-    //    // check file exists
-    //    helpers.assertFile('app/model/post.js');
-    //    // Check content
-    //    helpers.assertFile('app/model/post.js', /Backbone\.model/);
-    it('should create expected files');
+    this.webapp.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
+  });
 
+  it('creates expected files in AMD mode', function (done) {
+    var expected= [
+      ['component.json', /"name": "temp"/],
+      ['package.json', /"name": "temp"/],
+      'Gruntfile.js',
+      'app/404.html',
+      'app/favicon.ico',
+      'app/robots.txt',
+      'app/index.html',
+      ['app/scripts/main.js', /require\.config/],
+      'app/styles/main.scss'
+    ];
+
+    helpers.mockPrompt(this.webapp, {
+      'compassBootstrap': 'Y',
+      'includeRequireJS': 'Y'
+    });
+
+    this.webapp.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
   });
 });
