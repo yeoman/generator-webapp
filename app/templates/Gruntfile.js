a850f1bda3446b2df3134b0ccda2e6062a2ea56d
Pascal Hartig <phartig@rdrei.net>
2013-03-05 10:18:07 +0100

    Copy webp/gif to dist folder

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function (grunt) {
                     '<%%= yeoman.app %>/*.html',
                     '{.tmp,<%%= yeoman.app %>}/styles/{,*/}*.css',
                     '{.tmp,<%%= yeoman.app %>}/scripts/{,*/}*.js',
-                    '<%%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,webp}'
+                    '<%%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}'
                 ],
                 tasks: ['livereload']
             }
@@ -245,7 +245,8 @@ module.exports = function (grunt) {
                     dest: '<%%= yeoman.dist %>',
                     src: [
                         '*.{ico,txt}',
-                        '.htaccess'
+                        '.htaccess',
+                        'images/{,*/}*.{webp,gif}'
                     ]
                 }]
             }
