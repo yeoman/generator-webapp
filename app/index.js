7958a17bac7993b480ce4305b47a60edab22c013
Pascal Hartig <phartig@rdrei.net>
2013-06-11 12:15:04 +0300

    Use the new built-in Yeoman welcome message

--- app/index.js
+++ app/index.js
@@ -36,18 +36,7 @@ AppGenerator.prototype.askFor = function askFor() {
   var cb = this.async();
 
   // welcome message
-  var welcome =
-  '\n     _-----_' +
-  '\n    |       |' +
-  '\n    |'+'--(o)--'.red+'|   .--------------------------.' +
-  '\n   `---------´  |    '+'Welcome to Yeoman,'.yellow.bold+'    |' +
-  '\n    '+'( '.yellow+'_'+'´U`'.yellow+'_'+' )'.yellow+'   |   '+'ladies and gentlemen!'.yellow.bold+'  |' +
-  '\n    /___A___\\   \'__________________________\'' +
-  '\n     |  ~  |'.yellow +
-  '\n   __'+'\'.___.\''.yellow+'__' +
-  '\n ´   '+'`  |'.red+'° '+'´ Y'.red+' `\n';
-
-  console.log(welcome);
+  console.log(this.yeoman);
   console.log('Out of the box I include HTML5 Boilerplate, jQuery and Modernizr.');
 
   var prompts = [{
