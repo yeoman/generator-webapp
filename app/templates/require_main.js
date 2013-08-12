7258f91a6b9c97f0dc1078ea2df1f664575e9ee4
Pascal Hartig <phartig@rdrei.net>
2013-08-12 23:28:23 +0200

    Fixed requirejs + bootstrap
    Broke in 9bddba912184b12157c8b93f945574fe288d6442

--- app/templates/require_main.js
+++ app/templates/require_main.js
@@ -1,17 +1,56 @@
 require.config({
     paths: {
         jquery: '../bower_components/jquery/jquery'<% if (compassBootstrap) { %>,
-        bootstrap: 'vendor/bootstrap'<% } %>
+        bootstrapAffix: '../bower_components/sass-bootstrap/js/bootstrap-affix.js',
+        bootstrapAlert: '../bower_components/sass-bootstrap/js/bootstrap-alert.js',
+        bootstrapButton: '../bower_components/sass-bootstrap/js/bootstrap-button.js',
+        bootstrapCarousel: '../bower_components/sass-bootstrap/js/bootstrap-carousel.js',
+        bootstrapCollapse: '../bower_components/sass-bootstrap/js/bootstrap-collapse.js',
+        bootstrapPopover: '../bower_components/sass-bootstrap/js/bootstrap-popover.js',
+        bootstrapScrollspy: '../bower_components/sass-bootstrap/js/bootstrap-scrollspy.js',
+        bootstrapTab: '../bower_components/sass-bootstrap/js/bootstrap-tab.js',
+        bootstrapTooltip: '../bower_components/sass-bootstrap/js/bootstrap-tooltip.js',
+        bootstrapTransition: '../bower_components/sass-bootstrap/js/bootstrap-transition.js',
+        bootstrapTypeahead: '../bower_components/sass-bootstrap/js/bootstrap-typeahead.js'<% } %>
     }<% if (compassBootstrap) { %>,
     shim: {
-        bootstrap: {
-            deps: ['jquery'],
-            exports: 'jquery'
+        bootstrapAffix: {
+            deps: ['jquery']
+        },
+        bootstrapAlert: {
+            deps: ['jquery']
+        },
+        bootstrapButton: {
+            deps: ['jquery']
+        },
+        bootstrapCarousel: {
+            deps: ['jquery']
+        },
+        bootstrapCollapse: {
+            deps: ['jquery']
+        },
+        bootstrapPopover: {
+            deps: ['jquery']
+        },
+        bootstrapScrollspy: {
+            deps: ['jquery']
+        },
+        bootstrapTab: {
+            deps: ['jquery']
+        },
+        bootstrapTooltip: {
+            deps: ['jquery']
+        },
+        bootstrapTransition: {
+            deps: ['jquery']
+        },
+        bootstrapTypeahead: {
+            deps: ['jquery']
         }
     }<% } %>
 });
 
-require(['app', 'jquery'<% if (compassBootstrap) { %>, 'bootstrap'<% } %>], function (app, $) {
+require(['app', 'jquery'], function (app, $) {
     'use strict';
     // use app here
     console.log(app);
