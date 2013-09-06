7f6b1143fd0151fd885e3f1ae3a4b7c19d3e69d3
Pascal Hartig <phartig@rdrei.net>
2013-09-06 23:37:07 +0200

    build: copy bootstrap fonts
    Fixed #149
    
    If Bootstrap is enabled, this adds a copy task to just copy over the fonts from
    `bower_components` to the dist folder during the build.

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -309,7 +309,8 @@ module.exports = function (grunt) {
                         '*.{ico,png,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/{,*/}*.*'
+                        'styles/fonts/{,*/}*.*'<% if (compassBootstrap) { %>,
+                        'bower_components/sass-bootstrap/fonts/*.*'<% } %>
                     ]
                 }]
             },
