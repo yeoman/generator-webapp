aee916579d97d3ef10a1e93b5b4cc212b22bbaa1
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-26 02:35:37 +0100

    followup to #246

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -409,13 +409,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('server', function (target) {
         grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-
-        if (target) {
-            grunt.task.run(['serve:' + target]);
-        }
-        else {
-            grunt.task.run(['serve']);
-        }
+        grunt.task.run([target ? ('serve:' + target) : 'serve']);
     });
 
     grunt.registerTask('test', function(target) {
