83fd699db6de9ec932734444f3815ad813e09131
Chris Rebert <code@rebertia.com>
2014-02-22 18:21:33 -0800

    "Twitter Bootstrap" is now simply Bootstrap
    See http://getbootstrap.com/about/#brand

--- app/index.js
+++ app/index.js
@@ -117,7 +117,7 @@ AppGenerator.prototype.writeIndex = function writeIndex() {
   this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
   this.indexFile = this.engine(this.indexFile, this);
 
-  // wire Twitter Bootstrap plugins
+  // wire Bootstrap plugins
   if (this.includeBootstrap) {
     var bs = 'bower_components/bootstrap' + (this.includeSass ? '-sass/vendor/assets/javascripts/bootstrap/' : '/js/');
     this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
