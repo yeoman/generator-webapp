1139bb1a2c4cdb999b2a29ac57bdf672dd532a48
Matija Marohnić <matija.marohnic@gmail.com>
2014-04-07 17:16:47 +0200

    Remove unnecessary ignores from bowerInstall

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -236,14 +236,12 @@ module.exports = function (grunt) {
         // Automatically inject Bower components into the HTML file
         bowerInstall: {
             app: {
-                src: ['<%%= config.app %>/index.html'],
-                ignorePath: '<%%= config.app %>/',<% if (includeSass) { %>
+                src: ['<%%= config.app %>/index.html'],<% if (includeSass) { %>
                 exclude: ['bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap.js']<% } else { %>
                 exclude: ['bower_components/bootstrap/dist/js/bootstrap.js']<% } %>
             }<% if (includeSass) { %>,
             sass: {
-                src: ['<%%= config.app %>/styles/{,*/}*.{scss,sass}'],
-                ignorePath: '<%%= config.app %>/bower_components/'
+                src: ['<%%= config.app %>/styles/{,*/}*.{scss,sass}']
             }<% } %>
         },
 
