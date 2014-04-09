9cd81d9cbb636139f074ebbb2bab171144c3ba3c
Danmer <danmer.ekb@gmail.com>
2014-04-09 16:14:11 +0200

    Close GH-337: Remove unnecessary excludes and ignores.

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -236,9 +236,9 @@ module.exports = function (grunt) {
         // Automatically inject Bower components into the HTML file
         bowerInstall: {
             app: {
-                src: ['<%%= config.app %>/index.html'],<% if (includeSass) { %>
+                src: ['<%%= config.app %>/index.html']<% if (includeBootstrap) { %>,<% if (includeSass) { %>
                 exclude: ['bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap.js']<% } else { %>
-                exclude: ['bower_components/bootstrap/dist/js/bootstrap.js']<% } %>
+                exclude: ['bower_components/bootstrap/dist/js/bootstrap.js']<% } } %>
             }<% if (includeSass) { %>,
             sass: {
                 src: ['<%%= config.app %>/styles/{,*/}*.{scss,sass}']
