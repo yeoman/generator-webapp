e1035c5667ca32796c382b4715bb8e39f4712223
Matija Marohnić <matija.marohnic@gmail.com>
2014-07-25 15:58:10 +0200

    Fix the connect task
    Credits to @MMrko.

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -62,15 +62,16 @@ gulp.task('extras', function () {
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
 gulp.task('connect', function () {
-  var connect = require('connect');
-  var app = connect()
+  var serveStatic = require('serve-static');
+  var serveIndex = require('serve-index');
+  var app = require('connect')()
     .use(require('connect-livereload')({port: 35729}))
-    .use(connect.static('app'))
-    .use(connect.static('.tmp'))
+    .use(serveStatic('app'))
+    .use(serveStatic('.tmp'))
     // paths to bower_components should be relative to the current file
     // e.g. in app/index.html you should use ../bower_components
-    .use('/bower_components', connect.static('bower_components'))
-    .use(connect.directory('app'));
+    .use('/bower_components', serveStatic('bower_components'))
+    .use(serveIndex('app'));
 
   require('http').createServer(app)
     .listen(9000)
