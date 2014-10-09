058865289fc804ed8fff7bb40984d54e1964fb60
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-10-09 19:59:40 +0200

    Use libsass instead of ruby-sass
    libsass is faster and people don't have to install ruby.

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -6,7 +6,7 @@ var $ = require('gulp-load-plugins')();
 gulp.task('styles', function () {<% if (includeSass) { %>
   return gulp.src('app/styles/main.scss')
     .pipe($.plumber())
-    .pipe($.rubySass({
+    .pipe($.sass({
       style: 'expanded',
       precision: 10
     }))<% } else { %>
