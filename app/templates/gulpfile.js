b8bdfc5e173e7fb72c09d05a4c6c61876276c565
Matija Marohnić <matija.marohnic@gmail.com>
2014-11-05 12:59:24 +0100

    Set loose option to true for HTML minification

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('html', ['styles'], function () {<% if (includeBootstrap && includeSas
     .pipe($.if('*.css', $.csso()))<% } %>
     .pipe(assets.restore())
     .pipe($.useref())
-    .pipe($.if('*.html', $.minifyHtml({conditionals: true})))
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
     .pipe(gulp.dest('dist'));
 });
 
