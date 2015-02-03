c7bc12b4c5f280819de0325f790f7d82868fb3d3
Heath Morrison <heath@empathic.io>
2015-02-03 11:31:54 +0200

    Added woff2 to the list of font files to seek

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -53,7 +53,7 @@ gulp.task('images', function () {
 
 gulp.task('fonts', function () {
   return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
-    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+    .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
     .pipe($.flatten())
     .pipe(gulp.dest('.tmp/fonts'))
     .pipe(gulp.dest('dist/fonts'));
