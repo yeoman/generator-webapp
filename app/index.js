887d96bae2870d8b02fad8bcced2a1bb140a8d18
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-03-08 15:48:49 +0100

    Remove ruby Sass option
    Fix #484

--- app/index.js
+++ app/index.js
@@ -54,17 +54,6 @@ module.exports = yeoman.generators.Base.extend({
         value: 'includeModernizr',
         checked: false
       }]
-    }, {
-      when: function (answers) {
-        return answers && answers.features &&
-          answers.features.indexOf('includeSass') !== -1;
-      },
-      type: 'confirm',
-      name: 'libsass',
-      value: 'includeLibSass',
-      message: 'Would you like to use libsass? Read up more at \n' +
-        chalk.green('https://github.com/andrew/node-sass#node-sass'),
-      default: false
     }];
 
     this.prompt(prompts, function (answers) {
@@ -77,9 +66,6 @@ module.exports = yeoman.generators.Base.extend({
       this.includeSass = hasFeature('includeSass');
       this.includeBootstrap = hasFeature('includeBootstrap');
       this.includeModernizr = hasFeature('includeModernizr');
-
-      this.includeLibSass = answers.libsass;
-      this.includeRubySass = !answers.libsass;
 
       done();
     }.bind(this));
