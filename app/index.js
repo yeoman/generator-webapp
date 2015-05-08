c800f688ea37a3398746d02a1e4abccdb6382a6b
Brandon Dail <Aweary@users.noreply.github.com>
2015-05-08 10:15:06 -0500

    removed -official postfix from bootstrap-sass

--- app/index.js
+++ app/index.js
@@ -101,7 +101,7 @@ module.exports = yeoman.generators.Base.extend({
       };
 
       if (this.includeBootstrap) {
-        var bs = 'bootstrap' + (this.includeSass ? '-sass-official' : '');
+        var bs = 'bootstrap' + (this.includeSass ? '-sass' : '');
         bower.dependencies[bs] = '~3.3.1';
       } else {
         bower.dependencies.jquery = '~2.1.1';
