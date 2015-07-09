eab61846051b1b8c7d678ef3ca4e941f0d27e0fe
Matija Marohnić <matija.marohnic@gmail.com>
2015-07-09 15:25:44 +0200

    Update test framework generators
    - new generator-mocha plays better in headless browser workflows (e.g. PhantomJS)
    - new generator-jasmine installs dependencies in your existing bower.json and works with the latest version of Jasmine

--- app/index.js
+++ app/index.js
@@ -31,7 +31,7 @@ module.exports = generators.Base.extend({
     if (this.options['test-framework'] === 'mocha') {
       testLocal = require.resolve('generator-mocha/generators/app/index.js');
     } else if (this.options['test-framework'] === 'jasmine') {
-      testLocal = require.resolve('generator-jasmine/lib/generators/app/index.js');
+      testLocal = require.resolve('generator-jasmine/generators/app/index.js');
     }
 
     this.composeWith(this.options['test-framework'] + ':app', {
