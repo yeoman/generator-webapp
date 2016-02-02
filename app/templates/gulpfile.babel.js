40fc7da7dd754776c3a4ea2f9ae5c859db657fe2
Matija Marohnić <matija.marohnic@gmail.com>
2016-02-02 14:27:36 +0100

    Kill condition which prevented image minification
    Fixes #414.

--- app/templates/gulpfile.babel.js
+++ app/templates/gulpfile.babel.js
@@ -74,16 +74,12 @@ gulp.task('html', ['styles'], () => {
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
-    .pipe($.if($.if.isFile, $.cache($.imagemin({
+    .pipe($.cache($.imagemin({
       progressive: true,
       interlaced: true,
       // don't remove IDs from SVGs, they are often used
       // as hooks for embedding and styling
       svgoPlugins: [{cleanupIDs: false}]
-    }))
-    .on('error', function (err) {
-      console.log(err);
-      this.end();
     })))
     .pipe(gulp.dest('dist/images'));
 });
