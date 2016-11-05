311c681350f27986c5e0e25c537aee09b8e17a56
Matija Marohnić <matija.marohnic@gmail.com>
2016-11-05 19:54:00 +0100

    Reload AFTER linting fixes

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -42,8 +42,8 @@ gulp.task('scripts', () => {
 
 function lint(files, options) {
   return gulp.src(files)
-    .pipe(reload({stream: true, once: true}))
     .pipe($.eslint({ fix: true }))
+    .pipe(reload({stream: true, once: true}))
     .pipe($.eslint.format())
     .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
 }
