85b0be7b813de275da0837c1a2e83bd5773b3bd8
Cícero Pablo <cicero.pablos@gmail.com>
2017-07-08 09:09:03 -0300

    Use arrow function (#680)

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -86,7 +86,7 @@ gulp.task('images', () => {
 });
 
 gulp.task('fonts', () => {
-  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
+  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', () => {})
     .concat('app/fonts/**/*'))
     .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')));
 });
