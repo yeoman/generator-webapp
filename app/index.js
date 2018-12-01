17ef6e36c7e17b6490993b92bbde67f27768236d
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2018-12-01 19:31:08 +0000

    Refactor

--- app/index.js
+++ app/index.js
@@ -2,40 +2,23 @@
 const Generator = require('yeoman-generator');
 const commandExists = require('command-exists').sync;
 const yosay = require('yosay');
-const mkdirp = require('mkdirp');
+const helper = require('./lib/helper');
+const config = require('./lib/config');
 
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
-
-    this.option('skip-welcome-message', {
-      desc: 'Skips the welcome message',
-      type: Boolean
-    });
-
-    this.option('skip-install-message', {
-      desc: 'Skips the message after the installation of dependencies',
-      type: Boolean
-    });
-
-    this.option('test-framework', {
-      desc: 'Test framework to be invoked',
-      type: String,
-      defaults: 'mocha'
-    });
-
-    this.option('babel', {
-      desc: 'Use Babel',
-      type: Boolean,
-      defaults: true
-    });
+    for (let optionName in config.options) {
+      this.option(optionName, config.options[optionName]);
+    }
   }
 
   initializing() {
     this.pkg = require('../package.json');
     this.composeWith(
-      require.resolve(`generator-${this.options['test-framework']}/generators/app`),
-      { 'skip-install': this.options['skip-install'] }
+      require.resolve(`generator-${this.options['test-framework']}/generators/app`), {
+        'skip-install': this.options['skip-install']
+      }
     );
   }
 
@@ -44,32 +27,7 @@ module.exports = class extends Generator {
       this.log(yosay('\'Allo \'allo! Out of the box I include HTML5 Boilerplate, jQuery, and a gulpfile to build your app.'));
     }
 
-    const prompts = [{
-      type: 'checkbox',
-      name: 'features',
-      message: 'Which additional features would you like to include?',
-      choices: [{
-        name: 'Sass',
-        value: 'includeSass',
-        checked: true
-      }, {
-        name: 'Bootstrap',
-        value: 'includeBootstrap',
-        checked: true
-      }, {
-        name: 'Modernizr',
-        value: 'includeModernizr',
-        checked: true
-      }]
-    }, {
-      type: 'confirm',
-      name: 'includeJQuery',
-      message: 'Would you like to include jQuery?',
-      default: true,
-      when: answers => !answers.features.includes('includeBootstrap')
-    }];
-
-    return this.prompt(prompts).then(answers => {
+    return this.prompt(config.prompts).then(answers => {
       const features = answers.features;
       const hasFeature = feat => features && features.includes(feat);
 
@@ -84,146 +42,39 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this._writingModernizr();
-    this._writingGulpfile();
-    this._writingPackageJSON();
-    this._writingBabel();
-    this._writingGit();
-    this._writingEditorConfig();
-    this._writingH5bp();
-    this._writingStyles();
-    this._writingScripts();
-    this._writingHtml();
-    this._writingMisc();
-  }
+    const _self = this;
+    const data = {
+      appname: this.appname,
+      date: (new Date).toISOString().split('T')[0],
+      name: this.pkg.name,
+      version: this.pkg.version,
+      includeSass: this.includeSass,
+      includeBootstrap: this.includeBootstrap,
+      includeBabel: this.options.babel,
+      testFramework: this.options['test-framework'],
+      includeJQuery: this.includeJQuery,
+      includeModernizr: this.includeModernizr
+    };
 
-  _writingModernizr() {
-    if(this.includeModernizr) {
-      this.fs.copy(
-        this.templatePath('modernizr.json'),
-        this.destinationPath('modernizr.json')
-      );
-    }
-  }
-  _writingGulpfile() {
-    this.fs.copyTpl(
-      this.templatePath('gulpfile.js'),
-      this.destinationPath('gulpfile.js'),
-      {
-        date: (new Date).toISOString().split('T')[0],
-        name: this.pkg.name,
-        version: this.pkg.version,
-        includeSass: this.includeSass,
-        includeBootstrap: this.includeBootstrap,
-        includeBabel: this.options['babel'],
-        testFramework: this.options['test-framework']
-      }
-    );
-  }
+    // Render Files
+    config.filesToRender.forEach(file => {
+      helper.copyTpl.call(_self, file.input, file.output, data);
+    });
 
-  _writingPackageJSON() {
-    this.fs.copyTpl(
-      this.templatePath('_package.json'),
-      this.destinationPath('package.json'),
-      {
-        includeSass: this.includeSass,
-        includeBabel: this.options['babel'],
-        includeJQuery: this.includeJQuery,
-        includeBootstrap: this.includeBootstrap,
-        includeModernizr: this.includeModernizr
-      }
-    );
-  }
+    // Copy Files
+    config.filesToCopy.forEach(file => {
+      helper.copy.call(_self, file.input, file.output);
+    });
 
-  _writingBabel() {
-    this.fs.copy(
-      this.templatePath('babelrc'),
-      this.destinationPath('.babelrc')
-    );
-  }
+    // Create extra directories
+    config.dirToCreate.forEach(helper.createDirectory);
 
-  _writingGit() {
-    this.fs.copy(
-      this.templatePath('gitignore'),
-      this.destinationPath('.gitignore'));
-
-    this.fs.copy(
-      this.templatePath('gitattributes'),
-      this.destinationPath('.gitattributes'));
-  }
-
-  _writingEditorConfig() {
-    this.fs.copy(
-      this.templatePath('editorconfig'),
-      this.destinationPath('.editorconfig')
-    );
-  }
-
-  _writingH5bp() {
-    this.fs.copy(
-      this.templatePath('favicon.ico'),
-      this.destinationPath('app/favicon.ico')
-    );
-
-    this.fs.copy(
-      this.templatePath('apple-touch-icon.png'),
-      this.destinationPath('app/apple-touch-icon.png')
-    );
-
-    this.fs.copy(
-      this.templatePath('robots.txt'),
-      this.destinationPath('app/robots.txt'));
-  }
-
-  _writingStyles() {
-    let css = 'main';
-
-    if (this.includeSass) {
-      css += '.scss';
-    } else {
-      css += '.css';
+    if (this.includeModernizr) {
+      helper.copy.call(_self, 'modernizr.json', 'modernizr.json');
     }
 
-    this.fs.copyTpl(
-      this.templatePath(css),
-      this.destinationPath('app/styles/' + css),
-      {
-        includeBootstrap: this.includeBootstrap
-      }
-    );
-  }
-
-  _writingScripts() {
-    this.fs.copyTpl(
-      this.templatePath('main.js'),
-      this.destinationPath('app/scripts/main.js'),
-      {
-        includeBootstrap: this.includeBootstrap
-      }
-    );
-  }
-
-  _writingHtml() {
-    let bsPath, bsPlugins;
-
-    this.fs.copyTpl(
-      this.templatePath('index.html'),
-      this.destinationPath('app/index.html'),
-      {
-        appname: this.appname,
-        includeSass: this.includeSass,
-        includeBootstrap: this.includeBootstrap,
-        includeModernizr: this.includeModernizr,
-        includeJQuery: this.includeJQuery,
-        bsPath,
-        bsPlugins
-      }
-    );
-  }
-
-  _writingMisc() {
-    mkdirp('app/images');
-    mkdirp('app/fonts');
+    let cssFile = `main.${this.includeSass ? 'scss' : 'css'}`;
+    helper.copyTpl.call(_self, cssFile, `app/styles/${cssFile}`, data);
   }
 
   install() {
---
+++ app/lib/config.js
@@ -0,0 +1,93 @@
+module.exports = {
+  options: {
+    'skip-welcome-message': {
+      desc: 'Skips the welcome message',
+      type: Boolean
+    },
+    'skip-install-message': {
+      desc: 'Skips the message after the installation of dependencies',
+      type: Boolean
+    },
+    'test-framework': {
+      desc: 'Test framework to be invoked',
+      type: String,
+      defaults: 'mocha'
+    },
+    'babel': {
+      desc: 'Use Babel',
+      type: Boolean,
+      defaults: true
+    }
+  },
+  prompts: [{
+    type: 'checkbox',
+    name: 'features',
+    message: 'Which additional features would you like to include?',
+    choices: [{
+      name: 'Sass',
+      value: 'includeSass',
+      checked: true
+    }, {
+      name: 'Bootstrap',
+      value: 'includeBootstrap',
+      checked: true
+    }, {
+      name: 'Modernizr',
+      value: 'includeModernizr',
+      checked: true
+    }]
+  }, {
+    type: 'confirm',
+    name: 'includeJQuery',
+    message: 'Would you like to include jQuery?',
+    default: true,
+    when: answers => !answers.features.includes('includeBootstrap')
+  }],
+  dirToCreate: ['app/images', 'app/fonts'],
+  filesToCopy: [{
+      input: 'babelrc',
+      output: '.babelrc'
+    },
+    {
+      input: 'gitignore',
+      output: '.gitignore'
+    },
+    {
+      input: 'gitattributes',
+      output: '.gitattributes'
+    },
+    {
+      input: 'editorconfig',
+      output: '.editorconfig'
+    },
+    {
+      input: 'favicon.ico',
+      output: 'app/favicon.ico'
+    },
+    {
+      input: 'apple-touch-icon.png',
+      output: 'app/apple-touch-icon.png'
+    },
+    {
+      input: 'robots.txt',
+      output: 'app/robots.txt'
+    }
+  ],
+  filesToRender: [{
+      input: 'gulpfile.js',
+      output: 'gulpfile.js'
+    },
+    {
+      input: '_package.json',
+      output: 'package.json'
+    },
+    {
+      input: 'main.js',
+      output: 'app/scripts/main.js'
+    },
+    {
+      input: 'index.html',
+      output: 'app/index.html'
+    }
+  ]
+};
