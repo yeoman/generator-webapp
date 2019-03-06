cea838f344d9a4e691afbfd0b7b41a5315124288
Matija Marohnić <matija.marohnic@gmail.com>
2019-03-06 16:25:38 +0100

    Fix Modernizr tests

--- test/modernizr.js
+++ test/modernizr.js
@@ -11,31 +11,16 @@ describe('Modernizr feature', () => {
         .on('end', done);
     });
 
-    it('should add the correct tasks in package.json', () => {
-      assert.fileContent(
-        'package.json',
-        'modernizr -c modernizr.json -d app/scripts/modernizr.js'
-      );
-      assert.fileContent(
-        'package.json',
-        '"serve:test": "npm run build:modernizr &&'
-      );
-      assert.fileContent(
-        'package.json',
-        '"serve:dist": "npm run build:modernizr &&'
-      );
-      assert.fileContent(
-        'package.json',
-        '"start": "npm run build:modernizr && gulp serve'
-      );
-    });
-
     it('should add the correct files', () => {
       assert.file('modernizr.json');
     });
 
     it('should add the correct dependencies', () => {
       assert.fileContent('package.json', '"modernizr"');
+    });
+
+    it('should add the correct gulp task', () => {
+      assert.fileContent('gulpfile.js', 'function modernizr');
     });
   });
 
